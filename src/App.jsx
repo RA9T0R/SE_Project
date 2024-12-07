3a017d1cde7e0da47f4733b5535a0df6f8c0973a
RA9T0R <Phongphatbangkha@gmail.com>
2024-12-07 21:03:03 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,8 @@ const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">
-<<<<<<< Updated upstream
         Hello Fuck You!
-=======
         Hello Anusorn
->>>>>>> Stashed changes
       </h1>
       <Head/>
     </div>
